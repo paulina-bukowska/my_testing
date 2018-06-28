c8f885a9701db3db82e9cd1f36750d9fbc6f749d
paulawaster <pbukowska16@gmail.com>
2018-06-28 16:36:49 +0200

    Codility_task_8: PermCheck

---
+++ src/main/java/com/example/codility/PermCheck.java
@@ -0,0 +1,26 @@
+package com.example.codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermCheck {
+    public int solution(int[] A) {
+        Set<Integer> check = new HashSet<>();
+        int permutation = 1;
+
+        for (int num : A) {
+            check.add(num);
+        }
+
+        if (A.length != check.size()) {
+            return permutation = 0;
+        }
+
+        for (int i = 1; i <= check.size(); i++) {
+            if (!check.contains(i)) {
+                return permutation = 0;
+            }
+        }
+        return permutation;
+    }
+}
