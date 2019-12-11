549fcda48cbf9a8691cde5680e7e5e24c926f505
paulina-bukowska <pbukowska16@gmail.com>
2019-12-11 12:25:59 +0100

    sort

---
+++ src/test/java/com/example/codility/SelectionSortTestSuite.java
@@ -0,0 +1,32 @@
+package com.example.codility;
+
+import com.example.sort.SelectionSort;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SelectionSortTestSuite {
+    private SelectionSort selectionSort;
+
+    @Before
+    public void testSetup() throws Exception {
+        selectionSort = new SelectionSort();
+    }
+
+    @Test
+    public void testSelectionSort() {
+        //Given
+        int[] A = {5, 2, 8, 14, 1, 16};
+
+        //When
+        int[] sortedA = selectionSort.selectionSort(A);
+
+        //Then
+        Assert.assertEquals(1, sortedA[0]);
+        Assert.assertEquals(2, sortedA[1]);
+        Assert.assertEquals(5, sortedA[2]);
+        Assert.assertEquals(8, sortedA[3]);
+        Assert.assertEquals(14, sortedA[4]);
+        Assert.assertEquals(16, sortedA[5]);
+    }
+}
